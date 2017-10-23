736ac49d54d01fd50ea7f71e21f7ef2f6b2a81d9
tinchoas19 <martin@ctrlz.com.ar>
2017-10-23 19:06:20 -0300

    added video button

--- js/custom.js
+++ js/custom.js
@@ -31,8 +31,63 @@ var x = setInterval(function() {
 }, 1000);
 
 
+var player2;
+
+
+function onPlayerReady2(event) {
+  
+  // bind events
+  var playButton2 = document.getElementById("play-button2");
+  playButton2.addEventListener("click", function() {
+    playButton2.style.display = "none";
+    player2.playVideo();
+  });
+  
+
+}
+
+// global variable for the player
+var player;
+
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+  player2 = new YT.Player('video2', {
+    events: {
+      // call this function when player is ready to use
+      'onReady': onPlayerReady2
+    }
+  });
+  // create the global player from the specific iframe (#video)
+  player = new YT.Player('video', {
+    events: {
+      // call this function when player is ready to use
+      'onReady': onPlayerReady
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  
+  // bind events
+  var playButton = document.getElementById("play-button");
+  playButton.addEventListener("click", function() {
+    playButton.style.display = "none";
+    player.playVideo();
+  });
+  
+
+}
+
+// Inject YouTube API script
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
 $(document).ready(function(){
-  $(".graph-part").mouseover(function(){
+
+$(".graph-part").mouseover(function(){
        //console.log($(this).parent().parent().find(".graph-part").not(this));
        $(this).parent().parent().find(".graph-part").not(this).animate({
         opacity: 0.3
@@ -43,7 +98,7 @@ $(document).ready(function(){
 
      });
 
-  $(".graph-part").mouseout(function(){
+$(".graph-part").mouseout(function(){
        //console.log($(this).parent().parent().find(".graph-part").not(this));
        $(this).parent().parent().find(".graph-part").not(this).animate({
         opacity: 1
@@ -54,5 +109,38 @@ $(document).ready(function(){
        
      });
 
-  
+
+$('.mile-item').hover(function() {
+
+  $(this).stop().animate({
+    opacity: 1
+  }).parent().parent().parent().find(".mile-item").not(this).stop().animate({
+    opacity: 0
+  });
+}, function() {
+  $("#roadmap .mile-item").not(this).stop().animate({
+    opacity: 1
+  });
+});
+
+
+
+  /*$(".mile-item").mouseover(function(){
+    console.log("over item");
+    $(this).parent().parent().find(".mile-item").not(this).animate({
+      opacity: 0
+    }, 1000, function() {
+      $(".mile-item").mouseout(function(){
+       $(this).parent().parent().find(".mile-item").not(this).animate({
+        opacity: 1
+      }, 1000, function() {
+
+      });
+     });
+    });
+});*/
+
+
+
+
 });
